2425f0bb86cb7020644fb20e33de4ab9f941c66e
DuaLoad <asd101dsfp001sk@mail.ru>
2021-03-08 11:08:27 +0300

    Рефакторинг JS, добавлена механика работы тултипов

--- js/app.js
+++ js/app.js
@@ -1,13 +1,10 @@
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// !!!!!!!!!!!!!! ОСТОРОЖНО, НИЖЕ ЛЮТЫЙ ГОВНОКОД !!!!!!!!!!!!!!!!
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 // Генератор случайных чисел
 const randomNum = (min, max) => Math.floor(Math.random()*(max-min+1)+min),
       checked = 'overlay-check'; // Класс открытия оверлея
-
-/*document.getElementById('overlayClose').onclick = (e) => {
-    document.body.classList.remove(checked);
-};
-document.getElementById('overlay').onclick = (e) => {
-    document.body.classList.add(checked);
-};*/
 
 // Обработчик кликов для открытия/закрытия главного меню
 document.body.onclick = (e) => {
@@ -20,3 +17,43 @@ document.body.onclick = (e) => {
             break;
     }
 };
+
+// Реализация тултипов
+//
+// Элементу, на который хотим повесить тултип
+// добавляем класс tooltip-trigger
+//
+// data-title — заголовок тултипа
+// data-meta — описание, текст в тултипа
+// data-note — синяя нижняя строчка, заметка
+//
+let item = document.querySelectorAll('.tooltip-trigger');
+// Работает хер знает как (с божьей помощью)
+// Реализацию взял со своего старого проекта, вникать времени нет :-(
+item.forEach((item) =>{
+    // Получаем контейнер тултипа по ID
+    let tooltip = document.getElementById('tooltip');
+    // Накидываем события при наведении
+    item.addEventListener('mouseover', () =>{
+        // Заменяем содержание тултпиа на нужное
+        const replaceText = (selector, value) =>
+            tooltip.getElementsByClassName(selector)[0].innerHTML = value
+        // Проставляем заголовок
+        replaceText('title', item.dataset.title);
+        // Описание (текст) в тултипе
+        replaceText('meta', item.dataset.meta);
+        // Синяя "заметка", самая нижняя строчка
+        replaceText('note', item.dataset.note);
+    });
+    // Если мышь покинула элемент
+    // Отключаем тултип
+    item.addEventListener('mouseout', () =>{
+        tooltip.style.display="none";
+    });
+    // Перемещение тултипа за мышью
+    item.addEventListener('mousemove', (event) =>{
+        let y = event.clientY,
+            x = event.clientX + 15;
+        tooltip.style.cssText = `top:${y}px;left:${x}px`;
+    });
+});
