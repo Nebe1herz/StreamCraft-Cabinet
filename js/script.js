9ec80b7f6f1cd3d3e3e060b1651447f7bd7bd512
DuaLoad <asd101dsfp001sk@mail.ru>
2021-08-05 02:59:52 +0300

    Реализация механизма подгрузки содержимого модальных окон

--- js/script.js
+++ js/script.js
@@ -94,3 +94,43 @@ item.forEach((item) =>{
         tooltip.style.cssText = `top:${y}px;left:${x}px`;
     });
 });
+// Реализация модальных окон
+//
+// Элементу, который должен вызывать модальное окно
+// добавляем класс modal-trigger
+//
+//
+let modalTrigger = document.querySelectorAll('.modal-trigger');
+
+// Работает с божьей помощью, но работает ;-)
+modalTrigger.forEach((item) =>{
+    item.onclick = (i) => {
+        i.preventDefault();
+
+        console.log(i.target.getAttribute('data-modal-name'));
+
+        let request = new XMLHttpRequest();
+
+        // Открываем запрос
+        request.open('GET', '/[2021] SC Game Overlay/modal.donate.html');
+        // Отслеживание
+        request.onload = (e) => {
+            // Проверка на готовность загрузки страницы
+            if (request.readyState === 4) {
+                // Проверка успешности GET-запроса
+                switch(request.status) {
+                    case 200:
+                        //request.responseText
+                        break;
+                    default:
+                        console.error(request.statusText)
+                }
+            }
+        };
+
+        // Ловим ошибки
+        request.onerror = () => console.error(request.statusText);
+        request.send(null);
+
+    };
+});
