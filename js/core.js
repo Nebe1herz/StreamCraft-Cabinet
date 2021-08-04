dc20fd68a7bb933fa52c837834548eb26ebab167
DuaLoad <asd101dsfp001sk@mail.ru>
2021-08-05 00:57:11 +0300

    Весь JavaScript вынесен в отдельные файлы-модули по назначениям

---
+++ js/core.js
@@ -0,0 +1,29 @@
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// !!!!!!!!!!!!!! ОСТОРОЖНО, НИЖЕ ЛЮТЫЙ ГОВНОКОД !!!!!!!!!!!!!!!!
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// Генератор случайных чисел
+const randomNum = (min, max) => Math.floor(Math.random()*(max-min+1)+min),
+    checked = 'overlay-check'; // Класс открытия оверлея
+
+// Обработчик кликов для открытия/закрытия главного меню
+document.body.onclick = (e) => {
+    switch(e.target.classList[0]){
+        case 'overlay':
+            document.body.classList.toggle(checked);
+            break;
+        case 'overlay_close':
+            document.body.classList.remove(checked);
+            break;
+    }
+};
+
+
+// Обработчик нажатия клавиши Escape для открытия/закрытия главного меню
+document.addEventListener('keydown', (e) => {
+    switch(e.key){
+        case "Escape":
+            document.getElementsByClassName('overlay')[0].click();
+            break;
+    }
+});
