61d74fe2729e1b89e95c0b6dfd7a51228b6b4a4b
DuaLoad <asd101dsfp001sk@mail.ru>
2021-08-20 03:40:38 +0300

    Переработан скрипт срабатывания модального окна, внедрение делегирования

--- js/script.js
+++ js/script.js
@@ -188,23 +188,56 @@ const modalToggle = (action, content = null) => {
 }
 
 document.body.onclick = (e) => {
-    if(e.target.classList.contains('modal')) modalToggle('close');
-};
+    let item = e.target;
 
-// Работает с божьей помощью, но работает ;-)
-modalTrigger.forEach((item) =>{
-    item.onclick = (i) => {
-        i.preventDefault();
+    // Если клик по контейнеру modal или по любому внутреннему элементу
+    if(item.classList.contains('modal') || !!item.closest('.modal')) {
+        if(
+            // Если клик по modal-area (окно с контентом модалки)
+            item.classList.contains('modal-area')
+            // или
+            ||
+            // По любому внутреннему элементу modal-area
+            !!item.closest('.modal-area')
+            // То завершаем выполнение скрипт
+        ) return false;
 
-        if(i.target.classList.contains('modal-trigger-close')) {
+        // В ином случае закрываем модальное окно, поскольку если проверка не прошла
+        // Значит клик осуществлён вне окна с контентом модалки
+        modalToggle('close');
+    }
+
+    // Отработка открытия модалки по modal-trigger
+    if(
+        // Есть ли класс modal-trigger на элементе, по которому мы кликнули
+        item.classList.contains('modal-trigger')
+        // или
+        ||
+        // Является ли элемент, по которому мы кликнули, вложенным элементом в modal-trigger
+        !!item.closest('.modal-trigger')
+    ) {
+        // Отключаем срабатывание любого другого события
+        e.preventDefault();
+
+        // Если клик сработал на дочернем компоненте
+        // То переменной item переназначаем родительский элемент .modal-trigger
+        if(item.closest('.modal-trigger')){
+            item = item.closest('.modal-trigger');
+        }
+
+        // Проверка на наличие триггера для закрытия модалки
+        if(item.classList.contains('modal-trigger-close')) {
             modalToggle('close');
             return false;
         }
 
-        let modalName = i.target.getAttribute('data-modal-name');
-        modalToggle('show', modalName)
+        // Получаем название модалки, которую нужно вызвать
+        let modalName = item.dataset.modalName;
 
-    };
-});
+        // Вызываем модалку
+        modalToggle('show', modalName);
+    }
+};
 
-//document.addEventListener("DOMContentLoaded", modalToggle('show', 'modal.privilege.legend'));
+
+//document.addEventListener("DOMContentLoaded", modalToggle('show', 'modal.case.legend'));
