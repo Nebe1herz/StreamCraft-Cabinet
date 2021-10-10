5afcc23b84921c18cf67c9f560eaa0610da84422
DuaLoad <asd101dsfp001sk@mail.ru>
2021-10-10 22:20:27 +0300

    Рефакторинг модуля ratings.js, улучшена читаемость и оставлены комментарии

--- js/ratings.js
+++ js/ratings.js
@@ -1,10 +1,16 @@
-// Обработчик кликов
+// Механизм переключения вкладок на экране с рейтингами
+// - ratings.html
+//
+// Вешаем обработчик на документ
 document.body.onclick = (e) => {
+    // Проверяем, осуществлён ли клик по вкладке на экране с рейтингами
+    // Иначе останавливаем функцию
     if(!e.target.classList.contains('ratings-nav__item')) return false;
 
-    let classNav = 'ratings-nav__item',
-        classTab = 'ratings__tab-item',
-        rating = e.target.dataset.rating;
+    // Объявляем в переменные и назначаем им классы и аттрибуты
+    let classNav = 'ratings-nav__item', // Класс у вкладки в навигации
+        classTab = 'ratings__tab-item', // Класс у контейнера вкладки с содержимым
+        rating = e.target.dataset.rating; // ID рейтинга для открытия
 
     // Убираем checked состояние у всех остальных вкладок
     document.getElementsByClassName(`${classNav}_checked`)[0].classList.remove(`${classNav}_checked`)
@@ -15,40 +21,38 @@ document.body.onclick = (e) => {
     // Убираем класс checked у содержания вкладки
     document.getElementsByClassName(`${classTab}_checked`)[0].classList.remove(`${classTab}_checked`)
 
+    // Добавляем класс checked контейнеру с содержимым вкладки
+    // Который нам необходимо открыть
     document.querySelectorAll(`.${classTab}[data-rating="${rating}"]`)[0].classList.add(`${classTab}_checked`)
-
-    /*
-
-    //
-    document.querySelector(`${classTab}[data-rating*="${rating}"]`)[0].classList.add(`${classTab}_checked`)*/
 };
 
-//
+// Обработчик завершения загрузки DOM
 document.addEventListener("DOMContentLoaded", ()=>{
-    let
-        // Объект с элементами для перебора
+    let // Объект с элементами для перебора
         tableRow = document.getElementsByClassName('ratings__table-row'),
+        // Число, от которого считается статистика
+        // (отнимается для воспроизведения рейтинга)
         lastCount = 300;
 
-
     for (let i in tableRow) {
-        let
-            //
-            item = tableRow[i].children[1].childNodes[2];
+        let // Селектор для выделения
+            item = tableRow[i].children[1].childNodes[2],
             // Для расчётов динамики
             r = [
-            randomNum(0,1),
-            randomNum(0,1),
-            randomNum(0,1)
-        ];
+                randomNum(0,1),
+                randomNum(0,1),
+                randomNum(0,1)
+            ];
 
-        //
+        // Проверка на то, что существует ли такой элемент
+        // У заголовков его не должно быть и скрипт не выполнится
+        // TODO ПЕРЕПИСАТЬ ПРОВЕРКУ, здесь конченная логика
         if(item !== undefined) {
             // Голова скина
             tableRow[i].children[1].children[0].setAttribute('src', `images/heads/head-${randomNum(1, 12)}.png`)
 
             // Никнейм
-            item.textContent = nicknameArray[randomNum(0,nicknameArray.length)];
+            item.textContent = nicknameArray[randomNum(0,nicknameArray.length - 1)];
 
             // Количество убийств
             tableRow[i].children[2].textContent = `${lastCount-= randomNum(1, 15)} ${randomNum(111, 999)}`;
@@ -57,9 +61,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
             tableRow[i].children[3].textContent = `${Math.trunc(lastCount / randomNum(1, 15))} ${randomNum(111, 999)}`;
         }
 
+        // В заголовок тултипа пишем никнейм игрока
         tableRow[i].dataset.title = tableRow[i].children[1].innerText;
 
-        //
+        // Задаём тултипу описание
         tableRow[i].dataset.row = `
             <span class=dark-gray>Текущее место:</span>
             <span class=gold>${tableRow[i].children[0].innerText}</span>
