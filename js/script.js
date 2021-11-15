c3360a30ffa4aed00d82e349ddc4ac787b6bf71e
DuaLoad <asd101dsfp001sk@mail.ru>
2021-11-16 01:44:20 +0300

    Завершена реализация нового модуля profile-settings.js и сборка - Реализован механизм для имитации функционирования настроек на экране profile-settings.html; - Объявлен обработчик на контейнер .profile-settings и отслеживание клика на строчке с необходимым параметром; - Поскольку проект демонстрационный и сама механика модуля - костыль, то такими же костылями и велосипедами реализовано переключения значения у параметра между состояниями "Включено" и "Выключено", а также переключение этого во всплывающих подсказках и аттрибутах для их вызова; - Предусмотрен вариант переключения альтернативных значений, которые обоюдно имеют success-состояние; - Собраны, скомпилированы и минифицированы все JavaScript-модули.

--- js/script.js
+++ js/script.js
@@ -189,11 +189,11 @@ document.addEventListener('keydown', (e) => {
 
 // Реализация появления alert-оповещения
 
-// Элемент, который должен вызывать опощвение, назначем alert-trigger
+// Устанавливаем элементу .alert-trigger, который должен вызывать оповещение
 
 // data-alert-id — идентификатор уже реализованного уведомления
-// data-alert — пользовательское сообщение в оповещении, если не вызывается готовое
-// data-style — стилизация алерта (success, danger)
+// data-content — пользовательское сообщение в оповещении, если не вызывается готовое
+// data-style — стилизация оповещения (success, danger)
 
 let
     // Время совершения предыдущего события (клика)
@@ -542,6 +542,56 @@ document.body.onclick = (e) => {
         modalToggle(modalName);
     }
 };
+// Вешаем обработчик на документ
+document.getElementsByClassName('profile-settings')[0].addEventListener('click', (e) =>{
+    let
+        // Для сокращения записи объявляем item
+        item = e.target,
+        // Первый дочерний компонент "Выключено"
+        option1 = item.children[0],
+        // Второй дочерний компонент "Включено
+        option2 = item.children[1];
+
+    // Проверка, совершен ли клик по нужному элементу
+    if (
+        !(
+            // Произведен ли клик по элементу с нужным классом
+            item.classList.contains('profile-settings__item')
+            ||
+            // Или есть родитель, если клик прошёлся на дочерний компонент
+            item.closest('.profile-settings__item')
+        )
+    ) return -1;
+
+    // Переключаем у option1 и option2 классы .btn_danger и .btn_success
+    // При наличии аттрибута data-checked переключение происходит только .btn_success
+    option1.classList.toggle(
+        (item.hasAttribute('data-checked'))
+            ? 'btn_success'
+                : 'btn_danger'
+    );
+    option2.classList.toggle('btn_success');
+
+    // Если нет аттрибута data-checked
+    if(!item.hasAttribute('data-checked')){
+        // Если мы переключили значение на "Выключено"
+        if(option1.classList.contains('btn_danger')){
+            // В аттрибут, задающий содержание всплывающей подсказке, записываем новое значение
+            item.dataset.meta = `<span class=red>> ВЫКЛЮЧЕНО</span>`;
+            // Уже открытой всплывающей подсказке тоже обновляем значение
+            document.getElementsByClassName('tooltip__meta')[0].innerHTML
+                = `<span class=red>> ВЫКЛЮЧЕНО</span>`;
+        }
+        // Если мы переключили значение на "Включено"
+        else{
+            // В аттрибут, задающий содержание всплывающей подсказке, записываем новое значение
+            item.dataset.meta = `<span class=green>> ВКЛЮЧЕНО</span>`;
+            // Уже открытой всплывающей подсказке тоже обновляем значение
+            document.getElementsByClassName('tooltip__meta')[0].innerHTML
+                = `<span class=green>> ВКЛЮЧЕНО</span>`;
+        }
+    }
+});
 // Назначаем обработчик кликов элементу
 document.addEventListener('click', (e) => {
     // Контейнер со вкладками
